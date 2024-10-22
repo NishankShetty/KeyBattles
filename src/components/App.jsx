6bae6cfafe1aaab2aed9b406718f090efebe42b7
NishankShetty <nishankshetty2001@gmail.com>
2024-10-22 03:22:48 -0400

    Refactoring and Modularizing 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.scss";
+import Header from "./Header.jsx";
+import PlayersInfo from "./PlayersInfo.jsx";
+import "../App.scss";
 
 const sampleText =
   "examples of simple sentences include the following joe waited for the train was late mary and samantha took the bus";
@@ -14,66 +14,72 @@ function App() {
   const [userInput, setUserInput] = useState("");
   const [inputArray, setInputArray] = useState([]);
   const [cursorPosition, setCursorPosition] = useState(0);
+  const [inputLocked, setInputLocked] = useState(false);
   const wordsArray = wordCount(sampleText);
+  const inputLockedRef = useRef(false);
   const inputRef = useRef(null);
   const divRef = useRef(null);
-  const playersInfo = [
-    { userName: "Nishank", userId: 1001, progress: 75 },
-    { userName: "Adam", userId: 1002, progress: 50 },
-    { userName: "Charlie", userId: 1003, progress: 25 },
-    { userName: "Danny", userId: 1004, progress: 90 },
-  ];
+
+  let blurTimeout;
+
+  const freezeInput = () => {
+    setInputLocked(true);
+    inputLockedRef.current = true;
+    setTimeout(() => {
+      setInputLocked(false);
+      inputLockedRef.current = false;
+      console.log("Input Unfreezed.");
+    }, 2000);
+  };
 
   const handleText = (event) => {
-    // let input = event.target.value;
     console.log(event);
     let input = event;
-    // only alphabets,space , ctrl c,v,x,a block
-    // add previous and current input to userInput state
-    // input = input.replace(/\s+/g, " "); //move this inside the setUserInput
-    setUserInput((prev) => {
-      switch (true) {
-        case input.key === "Backspace":
-          return prev.slice(0, -1);
-        case input.key === "1":
-          console.log("Power Up 1 Fired");
-          return prev;
-        case input.key === "2":
-          console.log("Power Up 2 Fired");
-          return prev;
-        case input.key === "3":
-          console.log("Power Up 3 Fired");
-          return prev;
-        case input.code === "Space":
-          return prev + " ";
-        case input.code.slice(0, 3) !== "Key":
-          return prev;
-        case /^[A-Za-z\s]+$/.test(input.key):
-          console.log(/^[A-Za-z\s]+$/.test(input));
-          console.log(input);
-          return (prev + input.key).replace(/\s+/g, " ");
+    if (!inputLockedRef.current) {
+      setUserInput((prev) => {
+        switch (true) {
+          case input.key === "Backspace":
+            return prev.slice(0, -1);
+          case input.key === "1":
+            console.log("Power Up 1 Fired");
+            freezeInput();
+            return prev;
+          case input.key === "2":
+            console.log("Power Up 2 Fired");
+            return prev;
+          case input.key === "3":
+            console.log("Power Up 3 Fired");
+            return prev;
+          case input.code === "Space":
+            return prev + " ";
+          case input.code.slice(0, 3) !== "Key":
+            return prev;
+          case /^[A-Za-z\s]+$/.test(input.key):
+            // console.log(/^[A-Za-z\s]+$/.test(input));
+            return (prev + input.key).replace(/\s+/g, " ");
 
-        default:
-          return prev;
-      }
-    });
-
-    setCursorPosition(userInput.length);
+          default:
+            return prev;
+        }
+      });
+      setCursorPosition(userInput.length);
+    }
   };
 
   useEffect(() => {
     const handleFocus = () => {
       console.log("Focused");
+      clearTimeout(blurTimeout);
       divRef.current.classList.remove("blur");
-      document.getElementById("outOfFocus").classList.add("hidden");
+      document.getElementById("outOfFocus").classList.remove("hidden");
       inputRef.current.addEventListener("keydown", handleText);
     };
 
     const handleBlur = () => {
       console.log("Blurred");
-      setTimeout(() => {
+      blurTimeout = setTimeout(() => {
         divRef.current.classList.add("blur");
-        document.getElementById("outOfFocus").classList.remove("hidden");
+        document.getElementById("outOfFocus").classList.add("hidden");
       }, 1000);
       inputRef.current.removeEventListener("keydown", handleText);
     };
@@ -97,29 +103,13 @@ function App() {
 
   useEffect(() => {
     setInputArray(wordCount(userInput));
+    console.log("inputLocked state changed:", inputLocked);
   }, [userInput]);
 
   return (
     <>
-      <h2>KeyRacer</h2>
-
-      <div className="PlayersInfo">
-        <h3>Players</h3>
-        {playersInfo.map((player, index) => (
-          <span className={"Player " + (index + 1)} key={index}>
-            <div className="userName-container">
-              <div className="userName">{player.userName}</div>
-            </div>
-            <div className="progress-bar-container">
-              <div
-                className="progress-bar"
-                style={{ width: `${player.progress}%` }}
-              ></div>
-            </div>
-            <div className="statistics-container">Stats</div>
-          </span>
-        ))}
-      </div>
+      <Header />
+      <PlayersInfo />
       <div className="textArea" tabIndex="0" ref={inputRef}>
         <div
           className="outOfFocus"
@@ -136,7 +126,11 @@ function App() {
           <i className="fa-regular fa-hand-pointer"></i>Click Here To Focus
         </div>
 
-        <div className="displayText" ref={divRef}>
+        <div
+          className="displayText"
+          ref={divRef}
+          style={{ backgroundColor: inputLocked ? "lightblue" : "transparent" }}
+        >
           {wordsArray.map((word, wordIndex) => {
             return (
               <div
@@ -179,10 +173,10 @@ function App() {
       </div>
       <div className="powerUps-container">
         <button id="powerUpButton" className="powerUp One">
-          Power 1
+          Freeze 1
         </button>
-        <button className="powerUp Two">Power 2</button>
-        <button className="powerUp Three">Power 3</button>
+        <button className="powerUp Two">Bomb 2</button>
+        <button className="powerUp Three">Shield 3</button>
       </div>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function header() {
+  return (
+    <div>
+      <h2>KeyRacer</h2>
+    </div>
+  );
+}
